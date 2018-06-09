19c93bb8d241c406e2f57d30c61ac36807368ef3
Lewis Burns <lewis_b53b@hotmail.co.uk>
2018-06-09 23:03:26 +0100

    added basic Results page with text injected to Html using script tag

---
+++ public/webcrawler.js
@@ -0,0 +1,9 @@
+    var resultsPage = function(){
+
+    var textBox = document.createElement('p')
+
+    document.body.appendChild(textBox)
+    textBox.innerText = "Inserted Text"
+
+	}
+    window.addEventListener('load', resultsPage);
